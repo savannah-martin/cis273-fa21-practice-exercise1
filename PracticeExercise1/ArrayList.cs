8c8e8f0688723fec741817b2a824a47f72faa79d
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-09-28 10:00:46 -0500

    edit

--- PracticeExercise1/ArrayList.cs
+++ PracticeExercise1/ArrayList.cs
@@ -1,10 +1,109 @@
 ﻿using System;
 namespace PracticeExercise1
 {
-    public class ArrayList
+    public class ArrayList : IList
     {
+        private int[] array = null;
+        //count is how many of the array spots are in use
+        private int count = 0;
+
         public ArrayList()
         {
+            array = new int[16];
+
         }
+
+        //length not always count?
+        public int Length => count;
+
+        public bool IsEmpty => count == 0;
+
+        public void Append(int i)
+        {
+            if (count == array.Length)
+            {
+                Resize();
+            }
+
+            array[count++] = i;
+            //count++;
+        }
+
+        private void Resize()
+        {
+            //double length of array
+            Array.Resize(ref array, array.Length * 2);
+        }
+
+        public void InsertAfter(int newValue, int existingValue)
+        {
+            if (count == array.Length)
+            {
+                Resize();
+            }
+
+            int indexOfExistingValue = FirstIndexOf(existingValue);
+
+            if(indexOfExistingValue == -1)
+            {
+                Append(newValue);
+            }
+            else
+            {
+                //shift everything from that index to the right
+                ShiftRight(indexOfExistingValue);
+
+                //add new value at that index
+                array[indexOfExistingValue] = newValue;
+                count++;
+            }
+        }
+
+        public int FirstIndexOf(int existingValue)
+        {
+            //find index of existing value
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i] == existingValue)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Prepend(int i)
+        {
+            if (count == array.Length)
+            {
+                Resize();
+            }
+            ShiftRight(0);
+            array[0] = i;
+        }
+
+        private void ShiftRight(int startingIndex)
+        {
+            for (int i =count; i >startingIndex; i--)
+            {
+                array[i] = array[i - 1];
+            }
+        }
+
+        public override string ToString()
+        {
+            /*string result = "[";
+           for (int i=0; i<count -1; i++)
+            {
+                result += array[i] + ", ";
+            }
+
+            result += array[count - 1] + "]";
+            return result;*/
+
+            return "[" + String.Join(", ", array) + "]";
+        }
+
     }
 }
--- PracticeExercise1/IList.cs
+++ PracticeExercise1/IList.cs
@@ -3,5 +3,17 @@ namespace PracticeExercise1
 {
     public interface IList
     {
+        //Add i to the end of the list
+        void Append(int i);
+
+        //Add i to the beginning of the list
+        void Prepend(int i);
+
+        //Add i to the end of the list
+        void InsertAfter(int i);
+        int Length { get; }
+        bool IsEmpty { get; }
+        public int FirstIndexOf(int existingValue)
+
     }
 }
