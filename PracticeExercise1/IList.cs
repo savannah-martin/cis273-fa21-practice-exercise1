aa877ae767cc2334a0d0bd0dda142409eb94b195
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-09-28 09:12:59 -0500

    interface IList

--- PracticeExercise1/IList.cs
+++ PracticeExercise1/IList.cs
@@ -3,5 +3,10 @@ namespace PracticeExercise1
 {
     public interface IList
     {
+        //Add i to end of list
+        void Append(int i);
+        //Add i to beginning of list
+        void Prepend(int i);
+        void InsertAfter(int i);
     }
 }
