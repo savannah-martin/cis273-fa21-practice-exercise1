98a89d9d9604944987e246f3dd24da91566ad844
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-09-28 09:13:11 -0500

    Revert "interface IList"
    This reverts commit aa877ae767cc2334a0d0bd0dda142409eb94b195.

--- PracticeExercise1/IList.cs
+++ PracticeExercise1/IList.cs
@@ -3,10 +3,5 @@ namespace PracticeExercise1
 {
     public interface IList
     {
-        //Add i to end of list
-        void Append(int i);
-        //Add i to beginning of list
-        void Prepend(int i);
-        void InsertAfter(int i);
     }
 }
